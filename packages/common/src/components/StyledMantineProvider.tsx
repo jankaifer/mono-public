ab7c822b6689d70c5dbe37f2ab64a8f5716ec873
Jan Kaifer <jan@kaifer.dev>
2024-06-19 20:51:05 +0000

    Update

--- packages/common/src/components/StyledMantineProvider.tsx
+++ packages/common/src/components/StyledMantineProvider.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { MantineProvider, createTheme } from '@mantine/core'
-
 import { useMemo } from 'react'
 
 export const StyledMantineProvider = ({
@@ -12,6 +11,8 @@ export const StyledMantineProvider = ({
   theme: Parameters<typeof createTheme>[0]
 }) => {
   const generatedTheme = useMemo(() => createTheme(theme), [])
+  // @ts-expect-error arst
+  MantineProvider.test = 'was here'
 
   return (
     <MantineProvider defaultColorScheme="auto" theme={generatedTheme}>
